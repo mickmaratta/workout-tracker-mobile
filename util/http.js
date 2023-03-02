262909c7440645e73d0f3d99e0963babfcd934e2
Mick Maratta <mickmaratta@gmail.com>
2023-03-02 09:26:02 -0500

    Handled input changes

--- util/http.js
+++ util/http.js
@@ -5,3 +5,5 @@ const BACKEND_URL = "https://workout-app-ed4c7-default-rtdb.firebaseio.com";
 export async function storeUser(user) {
     const res = await axios.put(BACKEND_URL + `/users/${user.uid}.json`, user);
   }
+
+// ADD WORKOUT
